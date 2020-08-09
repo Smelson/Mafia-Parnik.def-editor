87cda0574b78d75e694126720c618052dfd4d56d
punks-mod <boss.vanek12@mail.ru>
2020-08-09 10:37:51 +0700

    Parnik.def Editor 1.0

---
+++ Project/Parnik.def/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic;
+
+namespace Parnik.def
+{
+
+    struct Parnikvar
+    {
+        public UInt32 FileSgn;
+        public UInt32 unknown;
+
+    }
+
+    struct ParnikCoords
+    {
+        public Single posX;
+        public Single posY;
+        public Single posZ;
+        public Single rotX;
+        public Single rotY;
+        public Single rotZ;
+        public List<Single> objNames;
+        public List<Single> rotlist;
+
+        public ParnikCoords(Single pX, Single pY, Single pZ, Single rX, Single rY, Single rZ, List<Single> objN, List<Single> rl)
+        {
+            posX = pX;
+            posY = pY;
+            posZ = pZ;
+            rotX = rX;
+            rotY = rY;
+            rotZ = rZ;
+            objNames = objN;
+            rotlist = rl;
+            
+        }
+    }
+
+
+
+    public partial class Form1 : Form
+    {
+        private Single positionX;
+        private Single positionY;
+        private Single positionZ;
+        private Single rotationX;
+        private Single rotationY;
+        private Single rotationZ;
+        List<ParnikCoords> ParnikCoords;
+        private List<Single> nameslist;
+        private List<Single> rotationlist;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "Mafia parnik.def file(*.def)|*.def";
+        }
+
+        string filename;
+        List<byte[]> Points = new List<byte[]>();
+        List<Single> Coords = new List<Single>();
+
+        Parnikvar PV = new Parnikvar();
+
+        private void openAFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel) { return; }
+            filename = openFileDialog1.FileName;
+            Coordinates.Items.Clear();
+            Rotation.Items.Clear();
+            ParnikCoords = new List<ParnikCoords>();
+            Coords.Clear();
+            Point.Items.Clear();
+
+            BinaryReader reader = new BinaryReader(File.Open(filename, FileMode.Open));
+            PV.FileSgn = reader.ReadUInt32();
+            PV.unknown = reader.ReadUInt32();
+
+            if (PV.FileSgn == 4)
+            {
+                filepath.Visible = true;
+                filepath.Text = filename;
+
+                for (int k = 0; k < PV.unknown * 4; k++)
+                {
+                    nameslist = new List<Single>();
+                    rotationlist = new List<Single>();
+                    positionX = reader.ReadSingle();
+                    nameslist.Add(positionX);
+                    Coords.Add(positionX);
+
+                    positionY = reader.ReadSingle();
+                    nameslist.Add(positionY);
+                    Coords.Add(positionY);
+
+                    positionZ = reader.ReadSingle();
+                    nameslist.Add(positionZ);
+                    Coords.Add(positionZ);
+
+
+                    rotationX = reader.ReadSingle();
+                    rotationlist.Add(rotationX);
+                    Coords.Add(rotationX);
+
+                    rotationY = reader.ReadSingle();
+                    rotationlist.Add(rotationY);
+                    Coords.Add(rotationY);
+
+                    rotationZ = reader.ReadSingle();
+                    rotationlist.Add(rotationZ);
+                    Coords.Add(rotationZ);
+                    ParnikCoords.Add(new ParnikCoords(positionX, positionY, positionZ, rotationX, rotationY, rotationZ, nameslist, rotationlist));
+                }
+
+                reader.Close();
+                VisualGroup();
+            }
+
+            else
+            {
+                MessageBox.Show("Couldn't parse a file!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                reader.Close();
+            }
+            
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(MousePosition, ToolStripDropDownDirection.Right);
+            }
+        }
+
+        private void editToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            int a = Point.SelectedIndex;
+            int i = Coordinates.SelectedIndex;
+            if (ParnikCoords != null && i >= 0 && a >= 0)
+            {
+                string newname = Interaction.InputBox("Type a new value of coordinate:", "New coord", Coordinates.SelectedItem.ToString());
+                if (newname == "")
+                    MessageBox.Show("Didn't change name!", "New coord", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                {
+                    Single newnamesingle = Convert.ToSingle(newname);
+                    Interaction.Beep();
+                    ParnikCoords[a].objNames[i] = newnamesingle;
+                    //VisualGroup();
+                    VisualObject(a);
+                }
+            }
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            int a = Point.SelectedIndex;
+            int i = Rotation.SelectedIndex;
+            if (ParnikCoords != null && i >= 0 && a >= 0)
+            {
+                string newname = Interaction.InputBox("Type a new value of coordinate:", "New coord", Rotation.SelectedItem.ToString());
+                if (newname == "")
+                    MessageBox.Show("Didn't change name!", "New coord", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                {
+                    Single newnamesingle = Convert.ToSingle(newname);
+                    Interaction.Beep();
+                    ParnikCoords[a].rotlist[i] = newnamesingle;
+                    //VisualGroup();
+                    VisualObject(a);
+                }
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Powered by Smelson! From Russia with love!", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BinaryWriter writer = new BinaryWriter(File.Open(filename, FileMode.Create));
+            writer.Write(PV.FileSgn);
+            writer.Write(PV.unknown);
+
+
+            for (int a = 0; a < ParnikCoords.Count; a++)
+            {
+
+                for (int i = 0; i < ParnikCoords[a].objNames.Count; i++)
+                {
+                    writer.Write(ParnikCoords[a].objNames[i]);
+                    i++;
+                    writer.Write(ParnikCoords[a].objNames[i]);
+                    i++;
+                    writer.Write(ParnikCoords[a].objNames[i]);
+
+                    for (int g = 0; g < ParnikCoords[a].rotlist.Count; g++)
+                    {
+                        writer.Write(ParnikCoords[a].rotlist[g]);
+                        g++;
+                        writer.Write(ParnikCoords[a].rotlist[g]);
+                        g++;
+                        writer.Write(ParnikCoords[a].rotlist[g]);
+                    }
+                }
+            }
+
+            writer.Close();
+        }
+
+        private void VisualGroup()
+        {
+            Point.Items.Clear();
+
+            if (ParnikCoords != null)
+            for (int a = 0; a < ParnikCoords.Count; a++)
+            Point.Items.Add((a + 1).ToString());
+        }
+
+        private void VisualObject(int ngr)
+        {
+            Coordinates.Items.Clear();
+            Rotation.Items.Clear();
+
+            if (ParnikCoords[ngr].objNames != null)
+                for (int i = 0; i < ParnikCoords[ngr].objNames.Count; i++)
+                {
+                    Coordinates.Items.Add(ParnikCoords[ngr].objNames[i]);
+                    Rotation.Items.Add(ParnikCoords[ngr].rotlist[i]);
+                }
+        }
+
+        private void Point_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            VisualObject(Point.SelectedIndex);
+        }
+
+
+    }
+}
---
+++ Src/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Parnik.def
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openAFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.Point = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Coordinates = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Rotation = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.filepath = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "parnik.def";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(667, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openAFileToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openAFileToolStripMenuItem
+            // 
+            this.openAFileToolStripMenuItem.Name = "openAFileToolStripMenuItem";
+            this.openAFileToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.openAFileToolStripMenuItem.Text = "Open A File";
+            this.openAFileToolStripMenuItem.Click += new System.EventHandler(this.openAFileToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Point
+            // 
+            this.Point.FormattingEnabled = true;
+            this.Point.Location = new System.Drawing.Point(0, 40);
+            this.Point.Name = "Point";
+            this.Point.Size = new System.Drawing.Size(126, 342);
+            this.Point.TabIndex = 1;
+            this.Point.SelectedIndexChanged += new System.EventHandler(this.Point_SelectedIndexChanged_1);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(95, 26);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(94, 22);
+            this.editToolStripMenuItem.Text = "Edit";
+            this.editToolStripMenuItem.Click += new System.EventHandler(this.editToolStripMenuItem_Click_1);
+            // 
+            // Coordinates
+            // 
+            this.Coordinates.ContextMenuStrip = this.contextMenuStrip1;
+            this.Coordinates.FormattingEnabled = true;
+            this.Coordinates.Location = new System.Drawing.Point(249, 40);
+            this.Coordinates.Name = "Coordinates";
+            this.Coordinates.Size = new System.Drawing.Size(126, 43);
+            this.Coordinates.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Points";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(279, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Coordinates";
+            // 
+            // Rotation
+            // 
+            this.Rotation.ContextMenuStrip = this.contextMenuStrip2;
+            this.Rotation.FormattingEnabled = true;
+            this.Rotation.Location = new System.Drawing.Point(541, 41);
+            this.Rotation.Name = "Rotation";
+            this.Rotation.Size = new System.Drawing.Size(126, 43);
+            this.Rotation.TabIndex = 5;
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip2.Name = "contextMenuStrip1";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(95, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(94, 22);
+            this.toolStripMenuItem1.Text = "Edit";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(577, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Rotation";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(229, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "X";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(229, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Y";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(229, 67);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Z";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(521, 68);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Z";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(521, 55);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(14, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Y";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(521, 42);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(14, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "X";
+            // 
+            // filepath
+            // 
+            this.filepath.AutoSize = true;
+            this.filepath.Location = new System.Drawing.Point(132, 359);
+            this.filepath.Name = "filepath";
+            this.filepath.Size = new System.Drawing.Size(44, 13);
+            this.filepath.TabIndex = 13;
+            this.filepath.Text = "Filepath";
+            this.filepath.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 381);
+            this.Controls.Add(this.filepath);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Rotation);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Coordinates);
+            this.Controls.Add(this.Point);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Parnik.def Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openAFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox Point;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ListBox Coordinates;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox Rotation;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Label filepath;
+    }
+}
+
